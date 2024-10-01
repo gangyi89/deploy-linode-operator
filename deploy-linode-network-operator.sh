eb627b68529174af5391d53c5b3dfd1598f5fbb5
Gang Yi Lim <galim@akamai.com>
2024-10-02 02:05:27 +0700

    update instructions

--- deploy-linode-network-operator.sh
+++ deploy-linode-network-operator.sh
@@ -9,7 +9,7 @@ if [ -n "$1" ]; then
 fi
 
 # Apply the YAML, replacing the namespace placeholder
-kubectl apply -f <(curl -s https://raw.githubusercontent.com/gangyi89/deploy-linode-operator/main/linode-network-operator.yaml | sed "s/\${NAMESPACE}/$NAMESPACE/") --namespace $NAMESPACE
+kubectl apply -f <(curl -s https://raw.githubusercontent.com/gangyi89/deploy-linode-operator/refs/heads/v2.0.0/linode-network-operator.yaml | sed "s/\${NAMESPACE}/$NAMESPACE/") --namespace $NAMESPACE
 
 echo ""
 echo "Linode Network Operator deployed to namespace: $NAMESPACE"
